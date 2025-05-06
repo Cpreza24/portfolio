b836a939dc8e20b1b1bbbb289bf01a1ee4d063e5
Cesar Preza <cpreza93@yahoo.com>
2025-05-05 20:15:26 -0700

    adds navbar styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.css';
 
 function Navbar({ scrollToProjects, scrollToContact }) {
-
   return (
-    <nav>
-        <button onClick={scrollToProjects}>Projects</button>
-        <button onClick={scrollToContact}>Contact</button>
+    <nav className="navbar">
+      <div className="navbar-left">
+        <h1 className="logo">Cesar Preza</h1>
+        <button onClick={scrollToProjects} className="nav-button">Projects</button>
+        <button onClick={scrollToContact} className="nav-button">Contact</button>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
