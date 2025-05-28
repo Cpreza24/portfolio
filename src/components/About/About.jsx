3109492ad0d23ab9c44d61ee614f698949c70fdc
Cpreza24 <cpreza93@yahoo.com>
2025-05-28 15:40:50 -0700

    adds about me section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,13 @@ function About() {
   return (
     <div>
       <h1>About</h1>
-      <p>Im Cesar, A Full-Stack Software Engineer based in Los Angeles, CA</p>
+      <p>Im Cesar, A Full-Stack Software Engineer based in Los Angeles, CA
+        I have a passion for building tools that make people’s lives easier. Wether it's helping users stay organized or simplifying a workflow. My journey into tech started with a long-standing love for the field, but a full-time career in healthcare kept it on the back burner. After 10 years working as an X-ray technician and medical assistant, I made the leap into software development through a full-time immersive bootcamp.
+
+        I specialize in JavaScript, React, and SQL-based databases, and enjoy building clean, functional applications that solve real problems. Though I dont shy away from learning new technologies and embrace the challenge. I’m confident working independently, but I also thrive in team environments. A skill honed over years of collaborating with clinical teams in high-pressure settings.
+
+        Now, I’m focused on growing as a developer and contributing to meaningful, user-focused applications in a full-stack role.
+      </p>
     </div>
   )
 }
